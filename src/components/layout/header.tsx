2bdbbc0166efd883941940dbad8f6e789b827144
Miguel Vila <miguelovila@ua.pt>
2025-02-25 01:33:04 +0000

    refactor: improve header and navigation component structure

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -13,13 +13,23 @@ const Shortcuts = () => (
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 flex w-full flex-row justify-center border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex items-center justify-between border-l border-r p-2">
-        <Logo />
-        <DesktopNavigationMenu />
-        <div className="flex h-full items-center gap-2">
+      <div className="container flex flex-1 items-center gap-2 border-l border-r p-2 lg:justify-between">
+        <div className="flex-grow basis-0">
+          <Logo />
+        </div>
+        <div className="hidden md:flex">
+          <DesktopNavigationMenu />
+        </div>
+        <Separator
+          orientation="vertical"
+          className="hidden h-2/3 md:max-lg:block"
+        />
+        <div className="flex h-full items-center justify-end gap-2 lg:flex-grow lg:basis-0">
           <Shortcuts />
           <Separator orientation="vertical" className="h-2/3 md:hidden" />
-          <MobileNavigationMenu />
+          <div className="md:hidden">
+            <MobileNavigationMenu />
+          </div>
         </div>
       </div>
     </header>
--- src/components/navigation/mobile.tsx
+++ src/components/navigation/mobile.tsx
@@ -14,31 +14,29 @@ import {
 import { navigationMobileMenuTriggerStyle } from "@/components/ui/navigation-menu";
 
 const MobileNavigationMenu = () => (
-  <div className="md:hidden">
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <LuMenu className="h-6 w-6" />
-        </Button>
-      </SheetTrigger>
-      <SheetContent className="flex w-[200px] flex-col items-center justify-center gap-2">
-        <SheetHeader>
-          <SheetTitle className="hidden">Mobile Navigation Bar</SheetTitle>
-        </SheetHeader>
-        {routes.map((route) => (
-          <NextLink
-            href={route.path}
-            key={route.path}
-            rel="noopener noreferrer"
-            aria-label={route.title}
-            className={navigationMobileMenuTriggerStyle()}
-          >
-            {route.title}
-          </NextLink>
-        ))}
-      </SheetContent>
-    </Sheet>
-  </div>
+  <Sheet>
+    <SheetTrigger asChild>
+      <Button variant="ghost" size="icon">
+        <LuMenu className="h-6 w-6" />
+      </Button>
+    </SheetTrigger>
+    <SheetContent className="flex w-[200px] flex-col items-center justify-center gap-2">
+      <SheetHeader>
+        <SheetTitle className="hidden">Mobile Navigation Bar</SheetTitle>
+      </SheetHeader>
+      {routes.map((route) => (
+        <NextLink
+          href={route.path}
+          key={route.path}
+          rel="noopener noreferrer"
+          aria-label={route.title}
+          className={navigationMobileMenuTriggerStyle()}
+        >
+          {route.title}
+        </NextLink>
+      ))}
+    </SheetContent>
+  </Sheet>
 );
 
 export default MobileNavigationMenu;
