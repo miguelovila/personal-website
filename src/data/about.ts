a0920f32278face0403acbfa5d3d34fed71f35bb
Miguel Vila <miguelovila@ua.pt>
2025-03-24 17:42:19 +0000

    feat: update education section and add experience details for clarity

--- src/data/about.ts
+++ src/data/about.ts
@@ -4,7 +4,7 @@ export const about =
 export const education = [
   {
     title: "Master's Degree in Computer and Telematics Engineering",
-    subtitle: "Universidade de Aveiro, Aveiro",
+    subtitle: "Universidade de Aveiro",
     start: "2024",
     end: "Present",
     description:
@@ -12,7 +12,7 @@ export const education = [
   },
   {
     title: "Bachelor's Degree in Computer Engineering and Telematics",
-    subtitle: "Universidade de Aveiro, Aveiro",
+    subtitle: "Universidade de Aveiro",
     start: "2021",
     end: "2024",
     description:
@@ -20,10 +20,29 @@ export const education = [
   },
   {
     title: "High School Diploma",
-    subtitle: "Escola Secundária de Estarreja, Estarreja",
+    subtitle: "Escola Secundária de Estarreja",
     start: "2018",
     end: "2021",
     description:
       "Graduated from Escola Secundária de Estarreja. I loved maths and phisics, and developed a passion for technology and programming as a self-taught hobby.",
+  },
+];
+
+export const experience = [
+  {
+    title: "Research Internship",
+    subtitle: "Instituto de Telecomunicações",
+    start: "2024",
+    end: "2025",
+    description:
+      "Working on the development of a platform for visualizing and analyzing historical data from the city of Aveiro.",
+  },
+  {
+    title: "Research Initiation Grant",
+    subtitle: "Instituto de Telecomunicações",
+    start: "2023",
+    end: "2024",
+    description:
+      "Joined the Newtwork Architectures and Protocols group working on the development of a platform for real-time monitoring of the city of Aveiro.",
   },
 ];
