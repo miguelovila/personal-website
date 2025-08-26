51d9d4bd4fc37bb124858158ba67771c16b599db
Miguel Vila <miguelovila@ua.pt>
2025-08-26 10:35:41 +0100

    refactor: preparing for transitioning to Astro.js

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import { guardEnv } from "guard-env";
-
-const env = guardEnv(process.env, {
-  NEXT_PUBLIC_BASE_URL: String,
-});
-
-export const BASE_URL = env.NEXT_PUBLIC_BASE_URL as string;
-export const OPEN_GRAPH_IMAGE = `${BASE_URL}/images/open-graph.webp`;
